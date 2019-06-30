6cc20c46b52b5e1d4134d57330957ad4727bfbc0
Ali A Eftekhari <e.eftekhari@gmail.com>
2019-06-30 22:21:59 +0200

    a figure for my presentation

--- compare_p_profile_steady.jl
+++ compare_p_profile_steady.jl
@@ -2,9 +2,11 @@
 using DataFrames
 df1 = readtable("p_profile_local_refine")
 df2 = readtable("p_profile_coarse_peaceman")
-figure()
-plot(df1[:x_m], df1[:p_pa], "--o", label="locally refined grid")
-plot(df2[:x_m], df2[:p_pa], label="coarse, Peaceman well model")
+figure(figsize=(5,2.5))
+plot(df1[:x_m], df1[:p_pa]./1e5, "--o", label="locally refined grid")
+plot(df2[:x_m], df2[:p_pa]./1e5, label="coarse, Peaceman well model")
 xlabel("x [m]")
-ylabel("p [Pa]")
+ylabel("p [bar]")
 legend()
+savefig("dp_t.png", bbox_inches="tight")
+
